a3673103b478116b458ffbe23a99c9b10efbf208
Adam m <adam@floorsmtihllc.com>
2019-11-29 20:07:46 -0500

    cleaned code and adjsted font

--- assets/logic.js
+++ assets/logic.js
@@ -5,6 +5,7 @@ var context;
 var topText;
 var canvas;
 var imgSrc;
+var topics2 = [];
 var topics = ["you will hold my beer", "yoda is so cute", ];
 // works fine just turned off for testing
 // function topicInfo() {
@@ -13,7 +14,6 @@ var topics = ["you will hold my beer", "yoda is so cute", ];
 //         "babyyoda";
 //     var q = $(this).attr("data-topic");
 //     var settings = {
-
 //         "async": true,
 //         "crossDomain": true,
 //         "url": "https://yodish.p.rapidapi.com/yoda.json?text=" + q,
@@ -44,6 +44,7 @@ function renderButtons() {
 //
 //$(document).on("click", ".topic-btn", topicInfo);
 renderButtons();
+// 
 $("#run-search").on("click", function (event) {
     event.preventDefault();
     var topic = $("#yoda-term").val();
@@ -51,10 +52,43 @@ $("#run-search").on("click", function (event) {
     renderButtons();
 });
 // end of yoda translator section
-
+// 
 // Meme collection section
-var topics2 = ["yoda", "baby yoda"];
-
+function imgGet() {
+    $("#trans-section").empty()
+    var topic2 = $("#meme-term").val();
+    var cors = "https://cors-anywhere.herokuapp.com/";
+    var queryURL = cors + "https://pixabay.com/api/?key=14379886-8edf494d6e4585af70ecf3230&q=" +
+        topic2 + "&image_type=photo";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        //for loop to generate images from api
+        for (let i = 0; i < 5; i++) {
+            var newImg = $("<img>").addClass("userMeme");
+            $(newImg).attr("src", response.hits[i].largeImageURL);
+            $("#meme-section").prepend(newImg);
+        };
+        //on click to select the user image
+        $(".userMeme").on("click", function () {
+            console.log(this)
+            $(this).addClass("selectedImg");
+            //function to put selected image into a canvas
+            function makeMeme() {
+                canvas = document.getElementById('myCanvas')
+                context = canvas.getContext('2d');
+                imgSrc = $('.selectedImg').attr('src');
+                make_base();
+                $("#trans-section").append();
+            };
+            makeMeme();
+            $("#meme-section").empty();
+        });
+    });
+};
+// 
 function make_base() {
     console.log("world");
     topText = $("#top-text").val();
@@ -65,67 +99,18 @@ function make_base() {
     context.fillStyle = "#ffffff";
     context.font = "160px Arial";
     console.log(base_image);
-
+    // 
     base_image.onload = function () {
         context.drawImage(base_image, 0, 0, 480, 480);
-        context.fillText(topText, -20, canvas.height + 20);
+        context.fillText(topText, 20, canvas.height - 360);
         context.fillText(bottomText, 20, canvas.height - 20);
     }
 }
-
-function imgGet() {
-    $("#trans-section").empty()
-    var topic2 = $("#meme-term").val();
-    var cors = "https://cors-anywhere.herokuapp.com/";
-    var queryURL = cors + "https://pixabay.com/api/?key=14379886-8edf494d6e4585af70ecf3230&q=" + topic2 + "&image_type=photo";
-
-    // var y = $(this).attr("meme-data");
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-
-        //for loop to generate images from api
-        for (let i = 0; i < 5; i++) {
-            var newImg = $("<img>").addClass("userMeme");
-            $(newImg).attr("src", response.hits[i].largeImageURL);
-            $("#meme-section").prepend(newImg);
-        };
-
-        //on click to select the user image
-        $(".userMeme").on("click", function () {
-            console.log(this)
-            $(this).addClass("selectedImg");
-
-            //function to put selected image into a canvas
-            function makeMeme() {
-
-                canvas = document.getElementById('myCanvas')
-                context = canvas.getContext('2d');
-                imgSrc = $('.selectedImg').attr('src');
-                make_base();
-
-                $("#trans-section").append();
-            };
-
-            makeMeme();
-
-            $("#meme-section").empty();
-        });
-
-    });
-};
 // 
-// 
-
 $("#top-btn").click(function () {
     console.log("testing");
     make_base();
-
 });
-
-// 
 //second button 
 $("#meme-search").on("click", function (event) {
     event.preventDefault();
@@ -157,33 +142,10 @@ $("#run-search").on("click", function (event) {
 //$(document).on("click", ".topic-btn", topicInfo);
 renderButtons();
 $(document).on("click", ".topic-btn", imgGet);
-
+// 
 function clear() {
     $("#meme-section").empty();
     $("#trans-section").empty();
 }
-// 
 $("#clear-all").on("click", clear);
-
-function renderImage(tr) {
-    $("#buttons-view").empty();
-    for (var i = 0; i < topics.length; i++) {
-        var a = $("<button>");
-        a.addClass("topic-btn");
-        a.attr("data-topic", topics[i]);
-        a.text(topics[i]);
-        $("#buttons-view").append(a);
-    }
-}
-// function make_base() {
-
-//     base_image = new Image();
-//     base_image.src = $('.selectedImg').attr('src');
-//     context.clearRect(0, 0, canvas.width, canvas.height);
-//     context.fillStyle = "#3e3e3e";
-//     context.font = "160px Arial";
-//     base_image.onload = function () {
-//         context.drawImage(base_image, 0, 0, 480, 480);
-//         // context.fillText(topText, 20, canvas.height - 20);
-//     }
-// }
+// 
